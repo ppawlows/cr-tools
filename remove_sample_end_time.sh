572045afd5fbdb660815f3f0dad5f689cb4978f1
Pawel Pawlowski <ppawlows@redhat.com>
2024-12-12 12:28:55 +0100

    remove sampleStartTime as well

--- remove_sample_end_time.sh
+++ remove_sample_end_time.sh
@@ -9,38 +9,10 @@ fi
 # Assign the provided URL to a variable
 url="$1"
 
-# Remove the sampleEndTime parameter
-modified_url=$(echo "$url" | sed -E 's/(&|\?)sampleEndTime=[^&]*//g' | sed -E 's/&{2,}/&/g' | sed -E 's/\?$//g')
+# Remove the sampleEndTime and sampleStartTime parameters
+modified_url=$(echo "$url" | sed -E 's/(&|\?)sampleEndTime=[^&]*//g' | sed -E 's/(&|\?)sampleStartTime=[^&]*//g' | sed -E 's/&{2,}/&/g' | sed -E 's/\?$//g')
 
-# Detect the OS and calculate 7 days ago at midnight
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS: Use BSD date
-  sample_start_time=$(date -v-7d '+%Y-%m-%d 00:00:00')
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # Linux: Use GNU date
-  if command -v date &>/dev/null && date --version 2>/dev/null | grep -q "GNU"; then
-    sample_start_time=$(date -d "-7 days" '+%Y-%m-%d 00:00:00')
-  else
-    echo "Error: Unsupported date command on Linux. Please use a GNU-compatible date command."
-    exit 1
-  fi
-else
-  echo "Unsupported OS: $OSTYPE"
-  exit 1
-fi
-
-# Replace or append sampleStartTime with the calculated value
-if [[ "$modified_url" =~ sampleStartTime= ]]; then
-  # Update existing sampleStartTime parameter
-  modified_url=$(echo "$modified_url" | sed -E "s/(sampleStartTime=)[^&]*/\1$(echo $sample_start_time | sed 's/ /%20/g')/")
-else
-  # Append sampleStartTime if it doesn't exist
-  separator="&"
-  [[ "$modified_url" != *"?"* ]] && separator="?"
-  modified_url="${modified_url}${separator}sampleStartTime=$(echo $sample_start_time | sed 's/ /%20/g')"
-fi
-
-# Determine the clipboard tool and copy the URL
+# Determine the OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
   echo "$modified_url" | pbcopy
@@ -58,7 +30,7 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     exit 1
   fi
 else
-  echo "Unsupported OS for clipboard copying: $OSTYPE"
+  echo "Unsupported OS: $OSTYPE"
   exit 1
 fi
 
